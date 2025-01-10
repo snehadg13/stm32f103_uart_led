62b8f2822ef3aa41f81dd371b89c1c4b307fb03f
snehadg13 <81614640+snehadg13@users.noreply.github.com>
2025-01-10 12:45:35 +0530

    Added led.h to LED folder

---
+++ LED/led.h
@@ -0,0 +1,5 @@
+#include "gpio.h"
+
+void LED_INIT(GPIO_config * Port, unsigned short int Pin);
+void LED_ON(GPIO_config * Port, unsigned short int Pin);
+void LED_OFF(GPIO_config * Port, unsigned short int Pin);
